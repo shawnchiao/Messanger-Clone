b632e777e4d31ff4bf00a5c53cb897a1deb483a1
Shawn Chiao <shawn1763@gmail.com>
2022-11-16 14:30:19 +1030

    Complete the structure of layout, Header

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/globals.css'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the MESSENGER</h1>
+    </div>
+  )
+}
+
+export default HomePage
